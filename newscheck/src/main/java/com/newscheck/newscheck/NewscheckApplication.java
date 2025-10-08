332128b5f848ee0ffa6419fb6c6f83fa303ba297
neoboi76 <dattimbol@mymail.mapua.edu.ph>
2025-10-09 00:27:50 +0800

    Set up project dependencies and initial files

---
+++ newscheck/src/main/java/com/newscheck/newscheck/NewscheckApplication.java
@@ -0,0 +1,13 @@
+package com.newscheck.newscheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewscheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewscheckApplication.class, args);
+	}
+
+}
---
+++ newscheck/src/test/java/com/newscheck/newscheck/NewscheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newscheck.newscheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewscheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
