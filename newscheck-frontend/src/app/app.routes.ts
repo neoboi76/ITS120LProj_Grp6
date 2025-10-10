769fd7a808e9fd3efa18ac8ba32c412f62f70133
neoboi76 <dattimbol@mymail.mapua.edu.ph>
2025-10-11 00:37:05 +0800

    Modify home page and build backend files

--- newscheck-frontend/src/app/app.routes.ts
+++ newscheck-frontend/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { Home } from './home/home';
 import { LogIn } from './components/log-in/log-in';
 import { Register } from './components/register/register';
+import { ResetPassword } from './components/reset-password/reset-password';
 
 export const routes: Routes = [
     { path: '', component: LogIn},
     { path: 'register', component: Register },
-    { path: 'home', component: Home }
+    { path: 'home', component: Home },
+    { path: 'reset-password', component: ResetPassword }
 ];
---
+++ newscheck-frontend/src/app/components/reset-password/reset-password.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-reset-password',
+  imports: [FormsModule],
+  templateUrl: './reset-password.html',
+  styleUrl: './reset-password.css'
+})
+export class ResetPassword {
+form: any;
+onSubmit() {
+throw new Error('Method not implemented.');
+}
+
+}
